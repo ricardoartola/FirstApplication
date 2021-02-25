792c3e04cfa257eb6a9c5d7edcba1a91ec088ce8
Ricardo Artola <rartola@outlook.com>
2021-02-25 12:27:11 -0500

    Add project files.

---
+++ FirstApplication/About.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Title="Menu Items" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="About.aspx.cs" Inherits="FirstApplication.About" %>
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <h2><%: Title %></h2>
+    <table>
+        <tr>
+            <td>
+                <asp:ListBox ID="lstSource" runat="server" Height="138px" Width="250px" SelectionMode="Multiple">
+                    <asp:ListItem>Pizza</asp:ListItem>
+                    <asp:ListItem>Hamburger</asp:ListItem>
+                    <asp:ListItem>French Fries</asp:ListItem>
+                    <asp:ListItem>Quesadilla</asp:ListItem>
+                </asp:ListBox></td>
+            <td>
+                <asp:Button ID="btnAllRight" runat="server" Text="&gt;&gt;" Width="50px" OnClick="btnAllRight_Click" /><br />
+                <asp:Button ID="btnSelectedRight" runat="server" Text="&gt; " Width="50px" OnClick="btnSelectedRight_Click" /><br />
+                <asp:Button ID="btnSelectedLeft" runat="server" Text="&lt; " Width="50px" OnClick="btnSelectedLeft_Click" /><br />
+                <asp:Button ID="btnAllLeft" runat="server" Text="&lt;&lt;" Width="50px" OnClick="btnAllLeft_Click" />
+            </td>
+            <td>
+                <asp:ListBox ID="lstDestination" runat="server" Height="134px" Width="250px" SelectionMode="Multiple"></asp:ListBox></td>
+        </tr>
+    </table>
+</asp:Content>
---
+++ FirstApplication/About.aspx.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace FirstApplication
+{
+    public partial class About : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnAllRight_Click(object sender, EventArgs e)
+        {
+            TransferAllItems(lstSource, lstDestination);
+        }
+        protected void btnAllLeft_Click(object sender, EventArgs e)
+        {
+            TransferAllItems(lstDestination, lstSource);
+        }
+
+        protected void btnSelectedRight_Click(object sender, EventArgs e)
+        {
+            TransferItems(lstSource, lstDestination);
+        }
+
+        protected void btnSelectedLeft_Click(object sender, EventArgs e)
+        {
+            TransferItems(lstDestination, lstSource);
+        }
+
+        private void TransferAllItems(ListBox lstFirst, ListBox lstSecond)
+        {
+            foreach (ListItem item in lstFirst.Items)
+                lstSecond.Items.Add(item);
+            lstFirst.Items.Clear();
+        }
+
+        private void TransferItems(ListBox lstFirst, ListBox lstSecond)
+        {
+            List<ListItem> ar1 = new List<ListItem>();
+            for (int i = 0; i < lstFirst.Items.Count; i++)
+            {
+                if (lstFirst.Items[i].Selected)
+                {
+                    if (!ar1.Contains(lstFirst.Items[i]))
+                        ar1.Add(lstFirst.Items[i]);
+                }
+            }
+            for (int i = 0; i < ar1.Count; i++)
+            {
+                if (!lstSecond.Items.Contains(ar1[i]))
+                    lstSecond.Items.Add(ar1[i]);
+                lstFirst.Items.Remove(ar1[i]);
+            }
+            lstSecond.SelectedIndex = -1;
+        }
+    }
+}
